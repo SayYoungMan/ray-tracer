86bc16d6eec77c2e7955f55f87d11c14d0610edc
SayYoungMan <nickte89@gmail.com>
2023-12-18 21:48:51 +0000

    refactor: grouped point and vector together

--- src/tuples.rs
+++ src/tuples.rs
@@ -1,36 +1,24 @@
 const EPSILON: f64 = 1e-5;
 
 #[derive(Debug)]
-struct Point(f64, f64, f64, f64);
+struct SpatialTuple(f64, f64, f64, f64);
 
-impl Point {
-    pub fn new(x: f64, y: f64, z: f64) -> Self {
+impl SpatialTuple {
+    pub fn new_point(x: f64, y: f64, z: f64) -> Self {
         Self(x, y, z, 1.0)
     }
-}
 
-impl PartialEq for Point {
-    fn eq(&self, other: &Self) -> bool {
-        (self.0 - other.0).abs() < EPSILON
-            && (self.1 - other.1).abs() < EPSILON
-            && (self.2 - other.2).abs() < EPSILON
-    }
-}
-
-#[derive(Debug)]
-struct Vector(f64, f64, f64, f64);
-
-impl Vector {
-    pub fn new(x: f64, y: f64, z: f64) -> Self {
+    pub fn new_vector(x: f64, y: f64, z: f64) -> Self {
         Self(x, y, z, 0.0)
     }
 }
 
-impl PartialEq for Vector {
+impl PartialEq for SpatialTuple {
     fn eq(&self, other: &Self) -> bool {
         (self.0 - other.0).abs() < EPSILON
             && (self.1 - other.1).abs() < EPSILON
             && (self.2 - other.2).abs() < EPSILON
+            && (self.3 - other.3).abs() < EPSILON
     }
 }
 
@@ -40,15 +28,15 @@ mod tests {
 
     #[test]
     fn point_creates_tuple_with_w1() {
-        let p = Point::new(4.0, -4.0, 3.0);
+        let p = SpatialTuple::new_point(4.0, -4.0, 3.0);
 
-        assert_eq!(p, Point(4.0, -4.0, 3.0, 1.0));
+        assert_eq!(p, SpatialTuple(4.0, -4.0, 3.0, 1.0));
     }
 
     #[test]
     fn vector_creates_tuple_with_w0() {
-        let v = Vector::new(4.0, -4.0, 3.0);
+        let v = SpatialTuple::new_vector(4.0, -4.0, 3.0);
 
-        assert_eq!(v, Vector(4.0, -4.0, 3.0, 0.0));
+        assert_eq!(v, SpatialTuple(4.0, -4.0, 3.0, 0.0));
     }
 }
