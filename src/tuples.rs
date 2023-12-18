b41c46b4f5fa085a3e1c8541241eac49c935f2c5
SayYoungMan <nickte89@gmail.com>
2023-12-18 22:03:03 +0000

    feat: able to subtract two SpatialTuples

--- src/tuples.rs
+++ src/tuples.rs
@@ -35,6 +35,19 @@ impl std::ops::Add for SpatialTuple {
     }
 }
 
+impl std::ops::Sub for SpatialTuple {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self::Output {
+        Self(
+            self.0 - other.0,
+            self.1 - other.1,
+            self.2 - other.2,
+            self.3 - other.3,
+        )
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -59,5 +72,32 @@ mod tests {
         let a2 = SpatialTuple(-2.0, 3.0, 1.0, 0.0);
 
         assert_eq!(a1 + a2, SpatialTuple(1.0, 1.0, 6.0, 1.0));
+    }
+
+    #[test]
+    fn subtracting_two_points() {
+        let p1 = SpatialTuple::new_point(3.0, 2.0, 1.0);
+        let p2 = SpatialTuple::new_point(5.0, 6.0, 7.0);
+
+        // Notice that you get a vector from subtracting two points
+        assert_eq!(p1 - p2, SpatialTuple::new_vector(-2.0, -4.0, -6.0));
+    }
+
+    #[test]
+    fn subtracting_vector_from_point() {
+        let p = SpatialTuple::new_point(3.0, 2.0, 1.0);
+        let v = SpatialTuple::new_vector(5.0, 6.0, 7.0);
+
+        // Conceptually, this is just moving backward by the given vector
+        assert_eq!(p - v, SpatialTuple::new_point(-2.0, -4.0, -6.0));
+    }
+
+    #[test]
+    fn subtracting_two_vectors() {
+        let v1 = SpatialTuple::new_vector(3.0, 2.0, 1.0);
+        let v2 = SpatialTuple::new_vector(5.0, 6.0, 7.0);
+
+        // This represents the change in direction between the two
+        assert_eq!(v1 - v2, SpatialTuple::new_vector(-2.0, -4.0, -6.0));
     }
 }
