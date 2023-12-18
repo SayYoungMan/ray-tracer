dee2d22e7a783cf928ad9b4a27d356b878cf8d0e
SayYoungMan <nickte89@gmail.com>
2023-12-18 21:39:23 +0000

    feat: compare tuples by checking if diff under epsilon

--- src/tuples.rs
+++ src/tuples.rs
@@ -1,4 +1,6 @@
-#[derive(Debug, PartialEq)]
+const EPSILON: f64 = 1e-5;
+
+#[derive(Debug)]
 struct Point(f64, f64, f64, f64);
 
 impl Point {
@@ -7,12 +9,28 @@ impl Point {
     }
 }
 
-#[derive(Debug, PartialEq)]
+impl PartialEq for Point {
+    fn eq(&self, other: &Self) -> bool {
+        (self.0 - other.0).abs() < EPSILON
+            && (self.1 - other.1).abs() < EPSILON
+            && (self.2 - other.2).abs() < EPSILON
+    }
+}
+
+#[derive(Debug)]
 struct Vector(f64, f64, f64, f64);
 
 impl Vector {
     pub fn new(x: f64, y: f64, z: f64) -> Self {
         Self(x, y, z, 0.0)
+    }
+}
+
+impl PartialEq for Vector {
+    fn eq(&self, other: &Self) -> bool {
+        (self.0 - other.0).abs() < EPSILON
+            && (self.1 - other.1).abs() < EPSILON
+            && (self.2 - other.2).abs() < EPSILON
     }
 }
 
