bfc462785ac3870a107f741d1d96ac10305ebeef
SayYoungMan <nickte89@gmail.com>
2023-12-20 15:50:57 +0000

    feat: inverse a matrix

--- src/matrices.rs
+++ src/matrices.rs
@@ -105,6 +105,32 @@ impl Matrix {
         return -self.minor(row, col);
     }
 
+    fn is_invertible(&self) -> bool {
+        self.determinant() != 0.0
+    }
+
+    pub fn inverse(&self) -> Self {
+        if !self.is_invertible() {
+            panic!("The following matrix is not invertible: {:#?}", self);
+        }
+
+        let det = self.determinant();
+        let mut data = Vec::new();
+        for j in 0..self.cols {
+            let mut row = Vec::new();
+            for i in 0..self.rows {
+                row.push(self.cofactor(i, j) / det);
+            }
+            data.push(row);
+        }
+
+        Matrix {
+            rows: self.rows,
+            cols: self.cols,
+            data,
+        }
+    }
+
     pub fn identity() -> Self {
         Matrix {
             rows: 4,
@@ -470,5 +496,110 @@ mod tests {
         assert_eq!(A.cofactor(0, 0), -12.0);
         assert_eq!(A.minor(1, 0), 25.0);
         assert_eq!(A.cofactor(1, 0), -25.0);
+    }
+
+    #[test]
+    fn testing_invertible_matrix_for_invertibility() {
+        let A = Matrix::from_vec(vec![
+            vec![6.0, 4.0, 4.0, 4.0],
+            vec![5.0, 5.0, 7.0, 6.0],
+            vec![4.0, -9.0, 3.0, -7.0],
+            vec![9.0, 1.0, 7.0, -6.0],
+        ]);
+
+        assert_eq!(A.determinant(), -2120.0);
+        assert_eq!(A.is_invertible(), true);
+    }
+
+    #[test]
+    fn testing_noninvertible_matrix_for_invertibility() {
+        let A = Matrix::from_vec(vec![
+            vec![-4.0, 2.0, -2.0, -3.0],
+            vec![9.0, 6.0, 2.0, 6.0],
+            vec![0.0, -5.0, 1.0, -5.0],
+            vec![0.0, 0.0, 0.0, 0.0],
+        ]);
+
+        assert_eq!(A.determinant(), 0.0);
+        assert_eq!(A.is_invertible(), false);
+    }
+
+    #[test]
+    fn calculating_inverse_of_matrix() {
+        let A = Matrix::from_vec(vec![
+            vec![-5.0, 2.0, 6.0, -8.0],
+            vec![1.0, -5.0, 1.0, 8.0],
+            vec![7.0, 7.0, -6.0, -7.0],
+            vec![1.0, -3.0, 7.0, 4.0],
+        ]);
+        let B = A.inverse();
+
+        assert_eq!(A.determinant(), 532.0);
+        assert_eq!(A.cofactor(2, 3), -160.0);
+        assert_eq!(B.at(3, 2), -160.0 / 532.0);
+        assert_eq!(A.cofactor(3, 2), 105.0);
+        assert_eq!(B.at(2, 3), 105.0 / 532.0);
+        assert_eq!(
+            B,
+            Matrix::from_vec(vec![
+                vec![0.21805, 0.45113, 0.24060, -0.04511],
+                vec![-0.80827, -1.45677, -0.44361, 0.52068],
+                vec![-0.07895, -0.22368, -0.05263, 0.19737],
+                vec![-0.52256, -0.81391, -0.30075, 0.30639],
+            ])
+        );
+
+        let A = Matrix::from_vec(vec![
+            vec![8.0, -5.0, 9.0, 2.0],
+            vec![7.0, 5.0, 6.0, 1.0],
+            vec![-6.0, 0.0, 9.0, 6.0],
+            vec![-3.0, 0.0, -9.0, -4.0],
+        ]);
+
+        assert_eq!(
+            A.inverse(),
+            Matrix::from_vec(vec![
+                vec![-0.15385, -0.15385, -0.28205, -0.53846],
+                vec![-0.07692, 0.12308, 0.02564, 0.03077],
+                vec![0.35897, 0.35897, 0.43590, 0.92308],
+                vec![-0.69231, -0.69231, -0.76923, -1.92308],
+            ])
+        );
+
+        let A = Matrix::from_vec(vec![
+            vec![9.0, 3.0, 0.0, 9.0],
+            vec![-5.0, -2.0, -6.0, -3.0],
+            vec![-4.0, 9.0, 6.0, 4.0],
+            vec![-7.0, 6.0, 6.0, 2.0],
+        ]);
+
+        assert_eq!(
+            A.inverse(),
+            Matrix::from_vec(vec![
+                vec![-0.04074, -0.07778, 0.14444, -0.22222],
+                vec![-0.07778, 0.03333, 0.36667, -0.33333],
+                vec![-0.02901, -0.14630, -0.10926, 0.12963],
+                vec![0.17778, 0.06667, -0.26667, 0.33333],
+            ])
+        );
+    }
+
+    #[test]
+    fn multiplying_product_by_its_inverse() {
+        let A = Matrix::from_vec(vec![
+            vec![3.0, -9.0, 7.0, 3.0],
+            vec![3.0, -8.0, 2.0, -9.0],
+            vec![-4.0, 4.0, 4.0, 1.0],
+            vec![-6.0, 5.0, -1.0, 1.0],
+        ]);
+        let B = Matrix::from_vec(vec![
+            vec![8.0, 2.0, 2.0, 2.0],
+            vec![3.0, -1.0, 7.0, 0.0],
+            vec![7.0, 0.0, 5.0, 4.0],
+            vec![6.0, -2.0, 0.0, 5.0],
+        ]);
+        let C = A.clone() * &B;
+
+        assert_eq!(C * B.inverse(), A);
     }
 }
