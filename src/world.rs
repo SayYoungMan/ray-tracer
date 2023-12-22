f8864ace62c8667d79a65aed2b50838fa1edf7a6
SayYoungMan <nickte89@gmail.com>
2023-12-22 20:51:30 +0000

    feat(worlds): intersect all objects of world with a ray

--- src/world.rs
+++ src/world.rs
@@ -1,5 +1,6 @@
 use crate::{
-    color::Color, lights::PointLight, sphere::Sphere, transformation::Transformation, tuples::Point,
+    color::Color, intersection::Intersection, lights::PointLight, rays::Ray, sphere::Sphere,
+    transformation::Transformation, tuples::Point,
 };
 
 pub struct World {
@@ -31,12 +32,25 @@ impl World {
             light,
         }
     }
+
+    pub fn intersect(&self, r: Ray) -> Vec<Intersection> {
+        let mut xs = Vec::new();
+        for object in self.objects.iter() {
+            xs.append(&mut object.intersect(r));
+        }
+
+        xs.sort_by(|a, b| a.t.partial_cmp(&b.t).unwrap());
+        xs
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::tuples::Vector;
+
     use super::*;
 
+    #[test]
     fn creating_world() {
         let w = World::new();
 
@@ -44,21 +58,17 @@ mod tests {
         assert_eq!(w.light.intensity, Color(0.0, 0.0, 0.0));
     }
 
-    fn default_world() {
-        let light = PointLight::new(Point::new(-10.0, 10.0, -10.0), Color(1.0, 1.0, 1.0));
-
-        let mut s1 = Sphere::origin_unit_sphere();
-        s1.material.color = Color(0.8, 1.0, 0.6);
-        s1.material.diffuse = 0.7;
-        s1.material.specular = 0.2;
-
-        let mut s2 = Sphere::origin_unit_sphere();
-        s2.set_transformation(vec![Transformation::Scaling(0.5, 0.5, 0.5)]);
-
+    #[test]
+    fn intersect_world_with_ray() {
         let w = World::default();
+        let r = Ray::new(Point::new(0.0, 0.0, -5.0), Vector::new(0.0, 0.0, 1.0));
 
-        assert_eq!(w.light, light);
-        assert!(w.objects.contains(&s1));
-        assert!(w.objects.contains(&s2));
+        let xs = w.intersect(r);
+
+        assert_eq!(xs.len(), 4);
+        assert_eq!(xs[0].t, 4.0);
+        assert_eq!(xs[1].t, 4.5);
+        assert_eq!(xs[2].t, 5.5);
+        assert_eq!(xs[3].t, 6.0);
     }
 }
